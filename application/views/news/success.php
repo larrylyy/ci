2ac11bffc69b04367ccb9745b83d0018c365ecdb
yuyong li <112203511@qq.com>
2012-05-02 21:18:20 +0800

    chapter 2 finished

---
+++ application/views/news/success.php
@@ -0,0 +1,17 @@
+<!--
+To change this template, choose Tools | Templates
+and open the template in the editor.
+-->
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+        <title></title>
+    </head>
+
+    <body>
+        <?php
+        echo 'success';
+        ?>
+    </body>
+</html>
