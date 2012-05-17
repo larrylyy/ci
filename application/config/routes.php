9ff3ea937e8e5900ef63ce1afc7d98df36e992b6
yuyong li <112203511@qq.com>
2012-05-17 15:25:58 +0800

    new chapter

--- application/config/routes.php
+++ application/config/routes.php
@@ -43,7 +43,7 @@
 $route['news/create'] = 'news/create';
 $route['news/(:any)'] = 'news/view/$1';
 $route['news'] = 'news';
-$route['(:any)'] = 'pages/view/$1';
+//$route['(:any)'] = 'pages/view/$1';
 $route['default_controller'] = 'pages/view';
 
 
